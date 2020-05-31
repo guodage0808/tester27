e1c385737747269f03181e1750288fbcfe7961cd
xiaopeng.guo <2171029637@qq.com>
2020-05-31 10:22:31 +0800

    add test code

---
+++ test/test01.py
@@ -0,0 +1,15 @@
+import time
+#使用ID定位
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('http://www.baidu.com')
+time.sleep(3)
+driver.find_element_by_id("kw").send_keys("test")
+time.sleep(2)
+driver.find_element_by_id("su").click()
+time.sleep(2)
+driver.find_element_by_link_text("百度首页").click()
+time.sleep(2)
+driver.find_element_by_id("kw").click()
+time.sleep(3)
+driver.find_element_by_xpath("//form[@id='form']/div/ul/li/span").click()
